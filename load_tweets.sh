76449d84190a525fec13130b5d3a2b6d8314f747
adamzterenyi <adamzterenyi@gmail.com>
2024-04-04 15:13:14 -0700

    normalized change

--- load_tweets.sh
+++ load_tweets.sh
@@ -9,7 +9,7 @@ test-data.zip
 
 echo 'load normalized'
 for file in $files; do
-    time python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:1362 --inputs "$file"
+    time python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:1363 --inputs "$file"
 done
 
 echo 'load denormalized'
